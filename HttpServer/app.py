bf460cf981c25aa3f9083f870d43cb03044123b9
wizroad <60961253+wizroad@users.noreply.github.com>
2021-09-14 14:46:55 +0900

    IMU sensor stream updated

--- HttpServer/app.py
+++ HttpServer/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request
 import cv2
 import numpy as np
+from socket import *
+import threading
+import struct
 
 app = Flask(__name__)
+
 
 width = 760
 height = 428
@@ -13,6 +17,33 @@ d_w = 320
 d_h = 288
 d_headersize = 17
 dnum = 0
+
+# UDP server thread - receive IMU sensor stream
+def UDPserver(ip, port):
+    serverSocket = socket(AF_INET, SOCK_DGRAM)
+    serverSocket.bind((ip, port))
+    dataformat = 'ffffqffffffffff'
+    data_name_list = ('MagZ', 'MagY', 'MagX', 'DeviceID', 'DeviceTime', 'ROTw', 'ROTz', 'ROTy', 'ROTx', 'Az', 'Ay', 'Ax', 'Gz', 'Gy', 'Gx')
+    print("server start")
+    while 1:
+        try:
+            data, info = serverSocket.recvfrom(2048)
+            print("recv data: %s bytes from %s/%d" % (len(data), info[0], info[1]))
+            if(len(data)==64): # IMU sensor stream format length
+                sensordata = struct.unpack(dataformat, data)
+                #####################################
+                # Write your code at here using data
+                # Sample code: print IMU sensor stream
+                #####################################
+                for i in range(len(data_name_list)):
+                    print("%s: %f / "%(data_name_list[i], sensordata[i]),end='')
+            else:
+                print("MSG is not IMU sensor stream")
+        except:
+            print("UDP packet receive error")
+            break
+
+    serverSocket.close()
 
 # Long throw depth stream
 @app.route('/DLT', methods=['POST'])
@@ -53,4 +84,11 @@ def Store():
     return 'Store Success'
 
 if __name__=='__main__':
-    app.run(host='192.168.0.3', port=5000)
+
+    server_ip = "192.168.0.3"
+    server_udp_port = 9005
+    server_http_port = 5000
+
+    t = threading.Thread(target=UDPserver, args=(server_ip, server_udp_port))
+    t.start()
+    app.run(host=server_ip, port=server_http_port)
